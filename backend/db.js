37b780508f69339836f99027248962e37b44109b
adiiityabisht <adityabisht227@gmail.com>
2023-07-13 01:04:56 +0530

    db connection and express server setup

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://127.0.0.1:27017/";
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to mongo successfully!");
+  } catch (error) {
+    console.error("Failed to connect to mongo:", error);
+  }
+};
+
+module.exports = connectToMongo;
