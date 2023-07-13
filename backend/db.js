2c25d3f3ffdda659be5be7aff2ca8073299d422a
adiiityabisht <adityabisht227@gmail.com>
2023-07-13 23:16:34 +0530

    Added data validation using express-validator

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://127.0.0.1:27017/";
+const mongoURI = "mongodb://127.0.0.1:27017/botenook";
 
 const connectToMongo = async () => {
   try {
     await mongoose.connect(mongoURI);
     console.log("Connected to mongo successfully!");
   } catch (error) {
-    console.error("Failed to connect to mongo:", error);
+    console.error("Failed to connect to mongo", error);
   }
 };
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,32 @@
-const express=require('express');
-const User=require('../models/User');
+const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
+// create a user using: POST "/api/auth" . No auth required
+router.post(
+  "/",
+  [
+    body("email", "Enter valid email").isEmail(),
+    body("name", "Enter valid name").isLength({ min: 3 }),
+    body("password", "Password must be  atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {console.log(err)
+    res.json(err.message)});
+  }
+);
 
-// create a user using: POST "/api/auth" . No auth required 
-router.post('/', (req,res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
-})
-
-module.exports = router
+module.exports = router;
